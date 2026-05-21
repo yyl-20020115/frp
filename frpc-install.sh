e9ead538830c1aac300cde77b0ab599c840fd27e
yyl-20020115 <yyl_20050115@hotmail.com>
2026-05-21 11:13:20 +0800

    changes

---
+++ frpc-install.sh
@@ -0,0 +1,4 @@
+sudo systemctl daemon-reload
+sudo systemctl enable frpc
+sudo systemctl start frpc
+
---
+++ frps-install.sh
@@ -0,0 +1,4 @@
+sudo systemctl daemon-reload
+sudo systemctl enable frps
+sudo systemctl start frps
+
